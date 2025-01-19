03efa7b6cec32566c204d5c9f8fea088b42ed09b
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-01-20 02:09:49 +0600

    create checkout form and load single data

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,11 +1,69 @@
-
+import { useLoaderData } from "react-router-dom";
 
 const CheckOut = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  const service = useLoaderData();
+  return (
+    <div className="mb-16">
+      <h2 className="text-3xl text-center font-bold my-6">
+        Booked Service :{" "}
+        <span className="text-secondary ">{service?.title}</span>
+      </h2>
+
+      <div className="card bg-base-100 w-full  shadow-2xl">
+        <form className="card-body">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CheckOut;
